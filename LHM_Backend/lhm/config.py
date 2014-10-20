f18eb0d7ebaaffe3c1f98b98923a1a907cf78e1b
Shantanu Bala <shantanubala@gmail.com>
2014-10-19 18:29:09 -0700

    added different database config for local dev

--- LHM_Backend/lhm/config.py
+++ LHM_Backend/lhm/config.py
@@ -1,10 +1,10 @@
 # configuration options
 
-DEBUG = True
+DEBUG = False
 PORT = 5000
 
 HOST = '127.0.0.1' # locally accessible IP address
 # HOST = '10.0.1.1'
 
-DATABASE_URL = "sqlite:////tmp/test.db"
+DATABASE_URL = "postgresql://lhm:asudevteam@localhost/lhm"
 SECRET_KEY = 'lhm'
--- LHM_Backend/lhm/runserver.py
+++ LHM_Backend/lhm/runserver.py
@@ -1,6 +1,9 @@
 from app import app
 from config import *
 
+DATABASE_URL = "sqlite:////tmp/test.db"
+DEBUG = True
+
 app.config.update(dict(
 	SQLALCHEMY_DATABASE_URI=DATABASE_URL,
 	DEBUG=DEBUG,
